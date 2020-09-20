6887a1fe9125243a76c4dc8f4896ed95c64157d4
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-21 00:25:45 +0900

    Update views.py

--- twitterCloneApp/views.py
+++ twitterCloneApp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from twitterCloneApp.models import TwUser, TwTweet
 from twitterCloneApp.forms import TwJoinForm, TwLoginForm
 from django.db.models import Count, Avg
+from django.contrib.auth import authenticate, login
 
 
 # Create your views here.
@@ -10,6 +11,29 @@ def main(request):
     return render(request, 'main.html', context)
 
 
-def join_form(request):
-    form = TwJoinForm()
-    return render(request, '/joinForm.html', {'form': form})
+def join(request):
+    if request.method == 'POST':
+        join_form = TwJoinForm(request.POST)
+        if join_form.is_valid():
+            new_item = join_form.save()
+            return redirect('main')
+    # pass
+    join_form = TwJoinForm()
+    return render(request, 'join.html', {'form': join_form})
+
+
+def user_login(request):
+    if request.method == 'POST':
+        login_form = TwLoginForm(request.POST)
+        username = request.POST['user_id']
+        password = request.POST['user_pwd']
+        print(request.POST)
+        user = authenticate(username=username, password=password)
+        print("1", user)
+        if user is not None:
+        # if login_form.is_valid():
+            login(request, user)
+            print("2", user)
+            return redirect('main')
+    login_form = TwLoginForm()
+    return render(request, 'login.html', {'form': login_form})
