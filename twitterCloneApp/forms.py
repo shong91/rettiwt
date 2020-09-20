02f7a9fd37e094ee12861e889deeda5b76f2e623
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-20 23:14:50 +0900

    update models and form
    - help_text
    - error messages
    
    set not forms.py but models.py... why?

--- twitterCloneApp/forms.py
+++ twitterCloneApp/forms.py
@@ -1,4 +1,3 @@
-# from django import forms
 from django import forms
 from django.forms import ModelForm
 from twitterCloneApp.models import TwUser
@@ -15,22 +14,22 @@ class TwJoinForm(ModelForm):
             , 'user_email': _('이메일 주소')
             , 'user_pwd': _('비밀번호')
         }
-        help_text = {
-            'user_id': _('아이디를 입력해주세요. ')
-            , 'user_nm': _('이름을 입력해주세요. ')
-            , 'user_email': _('이메일 주소를 입력해주세요. ')
-            , 'user_pwd': _('비밀번호를 입력해주세요. ')
-        }
+        # help_text = {
+        #     'user_id': _('아이디를 입력해주세요. ')
+        #     , 'user_nm': _('이름을 입력해주세요. ')
+        #     , 'user_email': _('이메일 주소를 입력해주세요. ')
+        #     , 'user_pwd': _('비밀번호를 입력해주세요. ')
+        # }
         widgets = {
             'user_pwd': forms.PasswordInput()
             , 'user_email': forms.EmailInput()
         }
-        error_messages = {
-            'user_id': _('아이디는 최대 30자로 설정 가능합니다. ')
-            , 'user_nm': _('이름은 최대 15자로 설정 가능합니다.  ')
-            , 'user_email': _('이메일 형식이 잘못되었습니다. ')
-            , 'user_pwd': _('비밀번호는 6자 이상 12자 이하, 영문+숫자 조합이어야 합니다.')
-        }
+        # error_messages = {
+        #     'user_id': _('아이디는 최대 30자로 설정 가능합니다. ')
+        #     , 'user_nm': _('이름은 최대 15자로 설정 가능합니다.  ')
+        #     , 'user_email': _('이메일 형식이 잘못되었습니다. ')
+        #     , 'user_pwd': _('비밀번호는 6자 이상 12자 이하, 영문+숫자 조합이어야 합니다.')
+        # }
 
 
 class TwLoginForm(ModelForm):
@@ -41,14 +40,14 @@ class TwLoginForm(ModelForm):
             'user_id': _('아이디')
             , 'user_pwd': _('비밀번호')
         }
-        help_text = {
-            'user_id': _('아이디를 입력해주세요. ')
-            , 'user_pwd': _('비밀번호를 입력해주세요. ')
-        }
+        # help_text = {
+        #     'user_id': _('아이디를 입력해주세요. ')
+        #     , 'user_pwd': _('비밀번호를 입력해주세요. ')
+        # }
         widgets = {
             'user_pwd': forms.PasswordInput()
         }
-        error_messages = {
-            'user_id': _('아이디는 최대 30자로 설정 가능합니다. ')
-            , 'user_pwd': _('비밀번호를 입력해주세요. ')
-        }
+        # error_messages = {
+        #     'user_id': _('아이디는 최대 30자로 설정 가능합니다. ')
+        #     , 'user_pwd': _('비밀번호를 입력해주세요. ')
+        # }
--- twitterCloneApp/models.py
+++ twitterCloneApp/models.py
@@ -1,4 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+error_messages = {
+    'user_id': _('아이디는 최대 30자로 설정 가능합니다. ')
+    , 'user_nm': _('이름은 최대 15자로 설정 가능합니다.  ')
+    , 'user_email': _('이메일 형식이 잘못되었습니다. ')
+    , 'user_pwd': _('비밀번호는 6자 이상 12자 이하, 영문+숫자 조합이어야 합니다.')
+    }
+
+help_text = {
+    'user_id': _('아이디를 입력해주세요. ')
+    , 'user_nm': _('이름을 입력해주세요. ')
+    , 'user_email': _('이메일 주소를 입력해주세요. ')
+    , 'user_pwd': _('비밀번호를 입력해주세요. ')
+}
 
 
 # Create your models here.
@@ -6,12 +21,12 @@ from django.db import models
 # Foreign key 설정 시 related_name 옵션에 대하여:
 # (Reference) https://velog.io/@brighten_the_way/Django%EC%99%80-Reverse-relations%EA%B3%BC-Relatedname
 class TwUser(models.Model):
-    user_id = models.CharField(max_length=30)
-    user_nm = models.CharField(max_length=45)
-    user_email = models.CharField(max_length=100)
+    user_id = models.CharField(max_length=30, help_text=help_text.get('user_id'), error_messages=error_messages)
+    user_nm = models.CharField(max_length=45, help_text=help_text.get('user_nm'), error_messages=error_messages)
+    user_email = models.CharField(max_length=100, help_text=help_text.get('user_email'), error_messages=error_messages)
     user_telno = models.CharField(max_length=12, default=None, null=True)
     user_birthday = models.CharField(max_length=8)
-    user_pwd = models.CharField(max_length=128)
+    user_pwd = models.CharField(max_length=128, help_text=help_text.get('user_pwd'), error_messages=error_messages)
     user_auth_yn = models.CharField(max_length=1, default="N")
     user_acc_pub_yn = models.CharField(max_length=1, default="Y")
     user_prof_pic = models.CharField(max_length=500)
@@ -26,9 +41,9 @@ class TwUser(models.Model):
 class TwFollowUser(models.Model):
     # follow_sno = models.IntegerField() # id가 있는데 이게 필요할까?
     # Many to One Relation: Foreign key 설정
-    user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, related_name="TwFollowUser_user_id")            # PK = FK (identifying-relation)
-    following_user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, null=True, related_name="following_user_id")  # PK != FK (Non-identifying-relation)
-    follower_user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, null=True, related_name="follower_user_id")   # PK != FK (Non-identifying-relation)
+    user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, related_name="TwFollowUser_user_id", error_messages=None)            # PK = FK (identifying-relation)
+    following_user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, default=None, null=True, related_name="following_user_id")  # PK != FK (Non-identifying-relation)
+    follower_user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, default=None, null=True, related_name="follower_user_id")   # PK != FK (Non-identifying-relation)
     data_del_yn = models.CharField(max_length=1, default="N")
     frt_user_id = models.CharField(max_length=30)
     frt_reg_date = models.DateTimeField(auto_now_add=True)
@@ -39,7 +54,7 @@ class TwFollowUser(models.Model):
 class TwTweet(models.Model):
     tw_sno = models.IntegerField()
     tw_psno = models.IntegerField()
-    user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, related_name="TwTweet_user_id")
+    user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, related_name="TwTweet_user_id", error_messages=None)
     tw_content = models.CharField(max_length=500)
     tw_image_url = models.CharField(max_length=500, default=None, null=True)
     tw_gif_url = models.CharField(max_length=500, default=None, null=True)
@@ -52,7 +67,7 @@ class TwTweet(models.Model):
 
 class TwAct(models.Model):
     tw_act_sno = models.IntegerField()
-    user_id = models.ForeignKey(TwTweet, on_delete=models.CASCADE, related_name="TwAct_user_id")
+    user_id = models.ForeignKey(TwTweet, on_delete=models.CASCADE, related_name="TwAct_user_id", error_messages=None)
     rt_user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, null=True, related_name="rt_user_id")
     qt_user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE, null=True, related_name="qt_user_id")
     like_user_id = models.ForeignKey(TwUser, on_delete=models.CASCADE)
@@ -61,3 +76,8 @@ class TwAct(models.Model):
     frt_reg_date = models.DateTimeField(auto_now_add=True)
     last_user_id = models.CharField(max_length=30)
     last_chg_date = models.DateTimeField(auto_now=True)
+
+
+# 모델 생성/수정 후 마이그레이션
+# python manage.py makemigrations
+# python manage.py migrate
