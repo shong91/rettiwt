a473a0173c0d3423fee95ad30ed22f7a7cbae0be
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-09-20 19:12:38 +0900

    update main page
    - load static files

--- twitterCloneApp/views.py
+++ twitterCloneApp/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from twitterCloneApp.models import TwUser, TwTweet
+from twitterCloneApp.forms import TwJoinForm, TwLoginForm
+from django.db.models import Count, Avg
 
 
 # Create your views here.
 def main(request):
     context = {}
     return render(request, 'main.html', context)
+
+
+def join_form(request):
+    form = TwJoinForm()
+    return render(request, '/joinForm.html', {'form': form})
