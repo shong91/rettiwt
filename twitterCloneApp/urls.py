76ab0e7a6e347a3069f9af9b0edf6763a855839c
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-10-03 19:39:41 +0900

    add: tweet CRUD

--- twitterCloneApp/urls.py
+++ twitterCloneApp/urls.py
@@ -5,10 +5,17 @@ app_name = 'twc'
 
 urlpatterns = [
     path('', views.main, name='main'),
+    # join/login
     path('join/', views.join, name='join'),
     path('login/', views.user_login, name='login'),
     path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
-    path('home/', views.get_list, name='home'),
-    path('tweet/', views.tweet, name='tweet')
+    path('logout/', views.logout, name='logout'),
+
+    # tweet CRUD
+    path('home/', views.list, name='home'),
+    path('tweet/', views.tweet, name='tweet'),
+    path('update/<int:id>', views.update, name='update'),
+    path('delete/<int:id>', views.delete, name='delete'),
+
 
 ]
