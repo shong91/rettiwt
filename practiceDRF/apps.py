b5617cae85d8c96cc2b368d3fbb17f13e4b19fce
Soyeon Hong <52073383+hhhongso@users.noreply.github.com>
2020-11-14 16:00:03 +0900

    DRF practice setting

---
+++ practiceDRF/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PracticedrfConfig(AppConfig):
+    name = 'practiceDRF'
---
+++ practiceDRF/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+
+# Create your views here.
+# ref) https://velog.io/@jcinsh/DRF-1-Setting-Serializer
